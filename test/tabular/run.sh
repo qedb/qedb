2ca6564c3beb4b0e0131b63ec5c2cdbda9ddaaea
Herman Bergwerf <hermanbergwerf@gmail.com>
2017-01-14 20:56:21 +0100

    Add header to test/tabular/run.sh

--- test/tabular/run.sh
+++ test/tabular/run.sh
@@ -1,3 +1,7 @@
-ssconvert -S ./test/data.gnumeric data.csv
-dart test/run.dart
-rm data.csv.*
+# Copyright (c) 2017, Herman Bergwerf. All rights reserved.
+# Use of this source code is governed by an AGPL-3.0-style license
+# that can be found in the LICENSE file.
+
+ssconvert -S ./test/tabular/data.gnumeric ./test/tabular/data.csv
+dart ./test/tabular/run.dart
+rm ./test/tabular/data.csv.*
