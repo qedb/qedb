28b6c973b6e9f93e7aa9de4c65515785c4562e9f
Herman Bergwerf <hermanbergwerf@gmail.com>
2017-01-19 01:10:05 +0100

    Remove sudo from kill-server.sh

--- tool/kill-server.sh
+++ tool/kill-server.sh
@@ -4,7 +4,7 @@
 # Use of this source code is governed by an AGPL-3.0-style license
 # that can be found in the LICENSE file.
 
-SERVER_PID=`sudo netstat -nltp | grep "dart" | awk '{print $7}' | awk -F / '{print $1}'`
+SERVER_PID=`netstat -nltp 2>/dev/null | grep "dart" | awk '{print $7}' | awk -F / '{print $1}'`
 if [ -n "${SERVER_PID}" ]
 then
   # Note that the server can handle SIGINT signals.
