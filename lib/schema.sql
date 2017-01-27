ad5f1fa2bbec88d986c6db753a941350a21d80ce
Herman Bergwerf <hermanbergwerf@gmail.com>
2017-01-27 14:21:46 +0100

    Update schema and test data

--- lib/schema.sql
+++ lib/schema.sql
@@ -15,7 +15,8 @@ CREATE EXTENSION pgcrypto;
 --
 -- Should NOT contain separate records for abbreviations or acronyms.
 CREATE TABLE descriptor (
-  id  serial  PRIMARY KEY
+  id          serial   PRIMARY KEY,
+  is_subject  boolean  NOT NULL
 );
 
 -- Locale
@@ -28,10 +29,20 @@ CREATE TABLE locale (
 -- Translation of a descriptor
 CREATE TABLE translation (
   id             serial   PRIMARY KEY,
-  descriptor_id  integer  NOT NULL UNIQUE REFERENCES descriptor(id),
+  descriptor_id  integer  NOT NULL REFERENCES descriptor(id),
   locale_id      integer  NOT NULL REFERENCES locale(id),
   content        text     NOT NULL
     CHECK (content ~ E'^(?:[^\\s]+ )*[^\\s]+$') -- Check for repeated spaces in the regex.
+  
+  -- Do NOT include a UNIQUE constraint, synonyms can now be stored by supplying
+  -- multiple translations for the same descriptor/locale pair.
+  --
+  -- Additionally, it is possible for different descriptors to have equal
+  -- translations. In this case their meaning can only be resolved by its
+  -- context (e.g. connected records). The addition of a duplicate translation
+  -- could be reviewed seperately to avoid descriptor duplication.
+  --
+  --UNIQUE (descriptor_id, locale_id)
 );
 
 --------------------------------------------------------------------------------
@@ -41,10 +52,12 @@ CREATE TABLE translation (
 -- Category
 CREATE TABLE category (
   id             serial     PRIMARY KEY,
-  parents        integer[]  NOT NULL
-);
+  parents        integer[]  NOT NULL,
 
--- Category name: reference decriptor
+  -- Category name
+  -- Must be a descriptor that is marked as subject.
+  descriptor_id  integer    REFERENCES descriptor(id)
+);
 
 -- Function
 CREATE TABLE function (
@@ -55,10 +68,64 @@ CREATE TABLE function (
   generic         boolean   NOT NULL CHECK (NOT generic OR argument_count < 2)
 );
 
--- Function subfield: reference descriptor
--- Function name: reference descriptor
--- Function tags: reference descriptor
--- Function keywords: reference keyword
+-- Function name
+CREATE TABLE function_name (
+  id             serial   PRIMARY KEY,
+  function_id    integer  NOT NULL REFERENCES function(id),
+  descriptor_id  integer  NOT NULL REFERENCES descriptor(id),
+
+  -- Names should uniquely identify a function.
+  UNIQUE (function_id, descriptor_id)
+);
+
+-- Function tag
+--
+-- The only difference with function_name is that there is no unique constraint
+-- (e.g. tags can be shared by multiple functions)
+-- Currently only descriptors that are marked as subject are allowed as tag.
+--
+-- Note that this is not a database for extensive information about functions.
+-- Tags should be kept minimal and primarily serve to enhance searching.
+CREATE TABLE function_tag (
+  id             serial   PRIMARY KEY,
+  function_id    integer  NOT NULL REFERENCES function(id),
+  descriptor_id  integer  NOT NULL REFERENCES descriptor(id)
+);
+
+CREATE TYPE keyword_type AS ENUM (
+  -- [a-z]+ form of an English translation of the function name descriptor
+  -- No upper caps allowed! If the original name contains spaces, dashes or
+  -- other complex symbols, you should consider to NOT create a 'word' keyword.
+  'word',
+  
+  -- Short form of the function name descriptor
+  'acronym',
+  'abbreviation',
+  
+  -- Related to the function LaTeX template
+  -- ([a-z]+ form of the function symbol)
+  'symbol'
+);
+
+-- Keywords ([a-z]+ sequences that identify the function)
+CREATE TABLE keyword (
+  id     serial        PRIMARY KEY,
+  value  text          NOT NULL CHECK (value ~ '^[a-z]+$'),
+  type   keyword_type  NOT NULL,
+
+  -- Equal values with different types are allowed.
+  UNIQUE (value, type)
+);
+
+-- Function keyword join
+CREATE TABLE function_keyword (
+  id           serial   PRIMARY KEY,
+  function_id  integer  NOT NULL REFERENCES function(id),
+  keyword_id   integer  NOT NULL REFERENCES keyword(id),
+
+  -- Do not repeat keywords.
+  UNIQUE (function_id, keyword_id)
+);
 
 -- Operator evaluation (relevant for printing parentheses)
 CREATE TYPE read_evaluation_type AS ENUM ('isolated', 'afirst', 'bfirst');
@@ -189,7 +256,7 @@ CREATE TABLE translocate (
 );
 
 --------------------------------------------------------------------------------
--- Emperical values and expression evaluation
+-- Empirical values and expression evaluation
 --------------------------------------------------------------------------------
 
 -- Empirical value reference source
@@ -198,15 +265,25 @@ CREATE TABLE empirical_reference (
   doi  text    NOT NULL UNIQUE
 );
 
--- Empirical values
-CREATE TABLE emperical_value (
+-- Empirical value (e.g. speed of light, or Avogadro's number)
+CREATE TABLE empirical_value (
   id             serial   PRIMARY KEY,
-  val            numeric  NOT NULL UNIQUE,
   expression_id  integer  REFERENCES expression(id),
-  reference_id   integer  REFERENCES empirical_reference(id)
+  reference_id   integer  REFERENCES empirical_reference(id),
+  val            numeric  NOT NULL UNIQUE
 );
 
-CREATE TYPE evaluation_parameter_type as ENUM ('empirical', 'computed');
+-- Pre-computed, theoretical value (e.g. Pi, or Phi, or e)
+-- Stored as 64bit floating point because that is the limit of computation.
+-- Theoretical values are considered exact and their digits beyond 64bit are
+-- not interesting.
+CREATE TABLE theoretical_value (
+  id             serial            PRIMARY KEY,
+  expression_id  integer           REFERENCES expression(id),
+  val            double precision  NOT NULL UNIQUE
+);
+
+CREATE TYPE evaluation_parameter_type as ENUM ('empirical', 'theoretical', 'evaluated');
 CREATE TYPE evaluation_parameter AS (
   ref   integer,
   type  evaluation_parameter_type
