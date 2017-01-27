a1828d564d6493fd83b0f1d597d8aa9d67372311
Herman Bergwerf <hermanbergwerf@gmail.com>
2017-01-27 14:20:46 +0100

    Format render.js correctly

--- web/views/render.js
+++ web/views/render.js
@@ -5,22 +5,22 @@
 var fs = require('fs')
 var pug = require('pug')
 var process = require('process')
-var readline = require('readline');
+var readline = require('readline')
 
 var fileName = process.argv[2]
 fs.readFile(fileName, (err, buf) => {
   if (err) {
-    throw err;
+    throw err
   }
-  var str = buf.toString('utf8');
-  var fn = pug.compile(str);
+  var str = buf.toString('utf8')
+  var fn = pug.compile(str)
 
   // Start listening on stdin.
   var lineReader = readline.createInterface({
     input: process.openStdin()
-  });
+  })
   lineReader.on('line', function (line) {
-    var json = JSON.parse(line);
-    process.stdout.write(fn(json));
-  });
-});
+    var json = JSON.parse(line)
+    process.stdout.write(fn(json))
+  })
+})
