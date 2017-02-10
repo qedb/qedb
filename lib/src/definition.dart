e164d48a67fb7d0444dcf43be51137641349e58d
Herman Bergwerf <hermanbergwerf@gmail.com>
2017-02-10 14:55:44 +0100

    Implement expression argument count check
    The current only entrypoint of externally generated expressions
    is definition/create.

--- lib/src/definition.dart
+++ lib/src/definition.dart
@@ -15,12 +15,13 @@ class CreateDefinition {
   String right;
 }
 
+/// Used to determine if an expression is valid under the given category.
 String queryIntersectFunctionIds(List<int> functionIds) => '''
-SELECT id FROM function WHERE category_id IN (
+SELECT id, argument_count FROM function WHERE category_id IN (
   SELECT unnest(array_append(parents, id))
   FROM category WHERE id = @categoryId)
 INTERSECT
-SELECT id FROM function WHERE id IN (${functionIds.join(',')})''';
+SELECT id, argument_count FROM function WHERE id IN (${functionIds.join(',')})''';
 
 Future<db.DefinitionTable> _createDefinition(
     Session s, CreateDefinition body) async {
@@ -42,15 +43,37 @@ Future<db.DefinitionTable> _createDefinition(
 
   // Retrieve all function IDs that are defined under this category.
   final allIds = leftData.functionId.toSet()..addAll(rightData.functionId);
-  final intersectResult = await s.conn.query(
-      queryIntersectFunctionIds(allIds.toList()),
+  // INTERSECT result
+  final intres = await s.conn.query(queryIntersectFunctionIds(allIds.toList()),
       {'categoryId': body.categoryId}).toList();
 
+  // Process INTERSECT result into a map.
+  final intmap = new Map<int, int>.fromIterable(intres,
+      key: (row) => row[0], value: (row) => row[1]);
+
   // Validate if all functions are defined in the context category.
-  if (intersectResult.length != allIds.length) {
-    final missing = allIds.difference(intersectResult.toSet());
+  if (intres.length != allIds.length) {
+    final missing = allIds.difference(intmap.keys.toSet());
     throw new UnprocessableEntityError(
         'given category does not contain some functions: $missing');
+  }
+
+  // Validate that the argument count defined in the expression headers matches
+  // the database.
+  for (var i = 0; i < leftData.functionArgc.length; i++) {
+    final id = leftData.functionId[i], argc = leftData.functionArgc[i];
+    if (intmap[id] != argc) {
+      throw new UnprocessableEntityError(
+          'left expression argument count array does not match the database');
+    }
+  }
+  // Ugly, but functional.
+  for (var i = 0; i < rightData.functionArgc.length; i++) {
+    final id = rightData.functionId[i], argc = rightData.functionArgc[i];
+    if (intmap[id] != argc) {
+      throw new UnprocessableEntityError(
+          'right expression argument count array does not match the database');
+    }
   }
 
   // Decode expressions.
