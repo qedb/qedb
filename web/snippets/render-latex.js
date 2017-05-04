7818854bbcfff5e1492be65811bfdeb2202ca684
Herman Bergwerf <hermanbergwerf@gmail.com>
2017-05-04 20:26:43 +0200

    Add some small UI changes

--- web/snippets/render-latex.js
+++ web/snippets/render-latex.js
@@ -2,12 +2,19 @@
 // Use of this source code is governed by an AGPL-3.0-style license
 // that can be found in the LICENSE file.
 
+var subopchars = {
+  '=': '{=}',
+  '^': '\\hat{}',
+  '~': '{\\sim}'
+};
+
 var spans = document.getElementsByClassName('latex')
 for (var i = 0; i < spans.length; i++) {
   var span = spans[i]
   var latex = span.innerText
-  latex = latex.replace(/(\$(?:(\d+)|\(([^\d]?)(\d+)([^\d]?)\)))/g, function (match, g1) {
-    return '\\mathit{\\' + g1 + '}'
+  latex = latex.replace(/(\$(?:(\d+)|\(([^\d]?)(\d+)([^\d]?)\)))/g, function (_, g1) {
+    var fixed = g1.replace(/([=^~])/g, function(_, g1) { return subopchars[g1]; });
+    return '\\mathsf{\\' + fixed + '}'
   })
   katex.render(latex, span, {displayMode: true})
 }
